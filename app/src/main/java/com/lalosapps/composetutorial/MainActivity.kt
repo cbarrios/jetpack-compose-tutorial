3116a649bd190b32c7c94b031aad8d4c204afc33
cbarrios <lalobarrios16@yahoo.com>
2022-04-25 15:07:09 -0400

    layouts: modifiers, Spacers

--- app/src/main/java/com/lalosapps/composetutorial/MainActivity.kt
+++ app/src/main/java/com/lalosapps/composetutorial/MainActivity.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lalosapps.composetutorial.domain.model.Message
 import com.lalosapps.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -42,13 +43,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = stringResource(id = R.string.contact_profile_picture)
+            contentDescription = stringResource(id = R.string.contact_profile_picture),
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
             Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
