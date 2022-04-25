68582917b4506869115a52daf0926b96d1c0296f
cbarrios <lalobarrios16@yahoo.com>
2022-04-25 14:47:34 -0400

    layouts: row, image, column

--- app/src/main/java/com/lalosapps/composetutorial/MainActivity.kt
+++ app/src/main/java/com/lalosapps/composetutorial/MainActivity.kt
@@ -3,13 +3,19 @@ package com.lalosapps.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.lalosapps.composetutorial.domain.model.Message
 import com.lalosapps.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MessageCard(
+                        Message(
+                            "Android",
+                            "Jetpack Compose"
+                        )
+                    )
                 }
             }
         }
@@ -30,14 +41,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = stringResource(id = R.string.contact_profile_picture)
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTutorialTheme {
-        Greeting("Android")
+        MessageCard(
+            Message(
+                "Colleague",
+                "Hey, take a look at Jetpack Compose, it's great!"
+            )
+        )
     }
 }
