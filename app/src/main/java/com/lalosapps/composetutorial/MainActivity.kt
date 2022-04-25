7187d47e74e078e1d244c29079da406f2b097bac
cbarrios <lalobarrios16@yahoo.com>
2022-04-25 17:10:16 -0400

    material: MaterialTheme, colors, shapes, typography, light and dark modes

--- app/src/main/java/com/lalosapps/composetutorial/MainActivity.kt
+++ app/src/main/java/com/lalosapps/composetutorial/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.lalosapps.composetutorial
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -45,30 +47,47 @@ class MainActivity : ComponentActivity() {
 fun MessageCard(msg: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
-            painter = painterResource(id = R.drawable.profile_picture),
+            painter = painterResource(R.drawable.profile_picture),
             contentDescription = stringResource(id = R.string.contact_profile_picture),
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
+
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(uiMode = UI_MODE_NIGHT_YES, name = "Dark Mode")
 @Composable
-fun DefaultPreview() {
+fun PreviewMessageCard() {
     ComposeTutorialTheme {
-        MessageCard(
-            Message(
-                "Colleague",
-                "Hey, take a look at Jetpack Compose, it's great!"
+        Surface(color = MaterialTheme.colors.background) {
+            MessageCard(
+                Message(
+                    "Colleague",
+                    "Hey, take a look at Jetpack Compose, it's great!"
+                )
             )
-        )
+        }
     }
 }
