2839ee00c63b89df6973b6714ef4fde4e46caf9f
cbarrios <lalobarrios16@yahoo.com>
2022-04-25 17:49:39 -0400

    list: LazyColumn, states, animation

--- app/src/main/java/com/lalosapps/composetutorial/MainActivity.kt
+++ app/src/main/java/com/lalosapps/composetutorial/MainActivity.kt
@@ -4,20 +4,26 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.lalosapps.composetutorial.data.local.SampleData
 import com.lalosapps.composetutorial.domain.model.Message
 import com.lalosapps.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -31,14 +37,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MessageCard(
-                        Message(
-                            "Android",
-                            "Jetpack Compose"
-                        )
-                    )
+                    Conversation(messages = SampleData.conversationSample)
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(name = "Dark Mode", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            Conversation(messages = SampleData.conversationSample)
         }
     }
 }
@@ -47,7 +68,7 @@ class MainActivity : ComponentActivity() {
 fun MessageCard(msg: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
-            painter = painterResource(R.drawable.profile_picture),
+            painter = painterResource(id = R.drawable.profile_picture),
             contentDescription = stringResource(id = R.string.contact_profile_picture),
             modifier = Modifier
                 .size(40.dp)
@@ -56,7 +77,12 @@ fun MessageCard(msg: Message) {
         )
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -65,11 +91,19 @@ fun MessageCard(msg: Message) {
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.body2
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                 )
             }
         }
@@ -77,7 +111,7 @@ fun MessageCard(msg: Message) {
 }
 
 @Preview(name = "Light Mode")
-@Preview(uiMode = UI_MODE_NIGHT_YES, name = "Dark Mode")
+@Preview(name = "Dark Mode", uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewMessageCard() {
     ComposeTutorialTheme {
